acba03183a4abbae3d03647e56ade8507d726131
ushiostarfish <ushiostarfish@gmail.com>
2018-09-29 18:12:21 +0900

    数値的テストを追加

---
+++ src/coordinate.hpp
@@ -0,0 +1,71 @@
+﻿#pragma once
+
+#include <cmath>
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+
+namespace rt {
+	inline glm::dvec3 polar_to_cartesian(double theta, double phi) {
+		double sinTheta = std::sin(theta);
+		glm::dvec3 v = {
+			sinTheta * std::cos(phi),
+			sinTheta * std::sin(phi),
+			std::cos(theta)
+		};
+		return v;
+	};
+	inline glm::dvec3 polar_to_cartesian(double cosTheta, double sinTheta, double phi) {
+		glm::dvec3 v = {
+			sinTheta * std::cos(phi),
+			sinTheta * std::sin(phi),
+			cosTheta
+		};
+		return v;
+	};
+
+	// Building an Orthonormal Basis, Revisited
+	template <typename Real, glm::precision P>
+	inline void orthonormalBasis(const glm::tvec3<Real, P>& zaxis, glm::tvec3<Real, P> *xaxis, glm::tvec3<Real, P> *yaxis) {
+		const Real sign = std::copysign(Real(1.0), zaxis.z);
+		const Real a = Real(-1.0) / (sign + zaxis.z);
+		const Real b = zaxis.x * zaxis.y * a;
+		*xaxis = glm::tvec3<Real, P>(Real(1.0) + sign * zaxis.x * zaxis.x * a, sign * b, -sign * zaxis.x);
+		*yaxis = glm::tvec3<Real, P>(b, sign + zaxis.y * zaxis.y * a, -zaxis.y);
+	}
+
+	// z が上, 任意の x, y
+	// 一般的な極座標系とも捉えられる
+	struct ArbitraryBRDFSpace {
+		ArbitraryBRDFSpace(const glm::dvec3 &zAxis) : zaxis(zAxis) {
+			orthonormalBasis(zAxis, &xaxis, &yaxis);
+		}
+		glm::dvec3 localToGlobal(const glm::dvec3 v) const {
+			/*
+			matrix
+			xaxis.x, yaxis.x, zaxis.x
+			xaxis.y, yaxis.y, zaxis.y
+			xaxis.z, yaxis.z, zaxis.z
+			*/
+			return v.x * xaxis + v.y * yaxis + v.z * zaxis;
+		}
+		glm::dvec3 globalToLocal(const glm::dvec3 v) const {
+			/*
+			matrix
+			xaxis.x, xaxis.y, xaxis.z
+			yaxis.x, yaxis.y, yaxis.z
+			zaxis.x, zaxis.y, zaxis.z
+			*/
+			return 
+				v.x * glm::dvec3(xaxis.x, yaxis.x, zaxis.x)
+				+ 
+				v.y * glm::dvec3(xaxis.y, yaxis.y, zaxis.y)
+				+ 
+				v.z * glm::dvec3(xaxis.z, yaxis.z, zaxis.z);
+		}
+
+		// axis on global space
+		glm::dvec3 xaxis;
+		glm::dvec3 yaxis;
+		glm::dvec3 zaxis;
+	};
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,87 @@
-#include "ofMain.h"
+﻿#include "ofMain.h"
 #include "ofApp.h"
+
+#define RUN_TEST 0
+
+#include "online.hpp"
+#include "peseudo_random.hpp"
+#include "coordinate.hpp"
+#include "randomsampler.hpp"
+// #include "SphericalRectangleSamplerCoordinate.hpp"
+#include "SphericalRectangleSamplerCoordinate_Optimized.hpp"
+
+/*
+ 任意の平面で、Area Uniformなサンプリングと、SolidAngle Uniformなサンプリングによる積分は、一致しなければならない。
+*/
+inline void simple_shading_check(int seed) {
+	rt::XoroshiroPlus128 random(seed);
+	glm::dvec3 o = glm::dvec3(random.uniform(-1.0, 1.0), random.uniform(-1.0, 1.0), random.uniform(-1.0, 1.0));
+	glm::dvec3 ex = rt::sample_on_unit_sphere(&random);
+
+	rt::Rectangle light = rt::Rectangle(
+		glm::dvec3(random.uniform(-1.0, 1.0), random.uniform(3.0, 4.0), random.uniform(-1.0, 1.0)),
+		ex * random.uniform(1.0, 2.0),
+		rt::ArbitraryBRDFSpace(ex).xaxis * random.uniform(1.0, 2.0)
+	);
+
+	rt::SphericalRectangleSamplerCoordinate sampler(light, o);
+
+	double sr = sampler.solidAngle();
+
+	rt::OnlineMean<double> Lo1;
+	rt::OnlineMean<double> Lo2;
+	glm::dvec3 Ng(0, 1, 0);
+	glm::dvec3 LN = light.normal();
+
+
+	printf("-- (seed: %d) --\n", seed);
+	printf("sr: %.4f\n", sr);
+	printf("area: %.4f\n", light.area());
+
+	for (int i = 0; i < 1000000; ++i) {
+		double Li = 10.0;
+		double brdf = 1.0 / glm::pi<double>();
+		{
+			glm::dvec3 sample = light.sample(random.uniform(), random.uniform()); /* area uniform */
+			glm::dvec3 wi = glm::normalize(sample - o);
+
+			double G = glm::dot(Ng, wi) * glm::abs(glm::dot(LN, -wi)) / glm::distance2(sample, o);
+			double pdf_area = 1.0 / light.area();
+			double value = Li * brdf * G / pdf_area;
+			Lo1.addSample(value);
+		}
+
+		{
+			glm::dvec3 sample = sampler.sample(random.uniform(), random.uniform()); /* solidangle uniform */
+			glm::dvec3 wi = glm::normalize(sample - o);
+			double pdf_sr = 1.0 / sampler.solidAngle();
+			double value = Li * brdf * glm::dot(Ng, wi) / pdf_sr;
+			Lo2.addSample(value);
+		}
+		// for graph check
+		// https://docs.google.com/spreadsheets/d/12isLUDUFe5WlRD9Zn6ByIJhgo4az62JZc6hNNvPx9b0/edit?usp=sharing
+		//if (i % 5000 == 0) {
+		//	printf("\"%.10f\",\"%.10f\",\"%.10f\"\n", Lo1.mean(), Lo2.mean(), Lo1.mean() - Lo2.mean());
+		//}
+	}
+	printf("%.5f, %.5f, d = %.5f\n", Lo1.mean(), Lo2.mean(), Lo1.mean() - Lo2.mean());
+
+	if (0.0005 < std::abs(Lo1.mean() - Lo2.mean())) {
+		// any bug detected
+		abort();
+	}
+}
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
-
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
+#if RUN_TEST
+	for (int i = 0; i < 100; ++i) {
+		simple_shading_check(i);
+	}
+	printf("done.\n");
+	std::cin.get();
+#else
+	ofSetupOpenGL(1024,768,OF_WINDOW);
 	ofRunApp(new ofApp());
-
+#endif
 }
